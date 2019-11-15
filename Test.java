b321947639c41fbaf0eb645487d2bdac99f24115
Guiller117 <gpc2120@gmail.com>
2019-11-15 15:38:40 -0500

    Initial commit

---
+++ Test.java
@@ -0,0 +1,19 @@
+
+
+public class Test{
+      public static void main(String[] args){
+            B b = new B();
+            System.out.println(b.getI());
+            System.out.println(b.getSuperI());
+
+            /*
+            A a;
+            a = new A();
+            a.display();
+            a = new B();
+            //a.display2();
+            B b = (B) a;
+            b.display2();
+            */
+      }
+}
